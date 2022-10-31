bead3cfb4b5bb8e595800045e40087094e68e16f
yuqian <987037574@qq.com>
2022-10-31 22:26:38 +0800

    bison

--- src/main/java/org/MyTest.java
+++ src/main/java/org/MyTest.java
@@ -1,6 +1,8 @@
 package org;
 
 import org.client.DbClient;
+import org.parser.Calc;
+import org.parser.CalcLexer;
 import org.util.MyMapFile;
 
 import java.io.*;
@@ -8,46 +10,35 @@ import java.net.URLDecoder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
+import static org.parser.Calc.YYRETURN;
+
 public class MyTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        try {
-            String writeFileName = DbClient.class.getResource("/2.txt").getPath();
-            writeFileName = URLDecoder.decode(writeFileName, "UTF-8");
+        String sql = "(1+2)*3/2\n";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(sql.getBytes());
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(byteArrayInputStream));
 
-            File writeFile = new File(writeFileName);
-            RandomAccessFile raf = new RandomAccessFile(writeFileName, "rw");
-
-
-            int count = 10;
-//            RandomAccessFile memoryMappedFile = new RandomAccessFile("D:\\1.txt", "rw");
-
-            System.out.println("writeFile");
-
-            byte[] byteArr = "hello".getBytes();
-
-            try {
-
-                MappedByteBuffer out = raf.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, count);
-
-                for (int i = 0; i < count; i++) {
-                    out.put((byte) i);
-                }
-                out.force();
-                raf.close();
-
-//                System.out.println(new String(byteArr));
-//                MappedByteBuffer map = raf.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, byteArr.length);
-////                map.position();
-//                map.put(byteArr);
-
-            }catch (IOException e) {
-                e.printStackTrace();
+        CalcLexer scanner = new CalcLexer(byteArrayInputStream);
+        Calc parser = new Calc(scanner);
+        for (String arg : args)
+            if (arg.equals("-p"))
+                parser.setDebugLevel(1);
+        int status;
+        do {
+            int token = scanner.getToken();
+            Object lval = scanner.getValue();
+            Calc.Location yyloc = scanner.getLocation();
+            status = parser.push_parse(token, lval, yyloc);
+            if(status == YYRETURN)
+            {
+                Object object = scanner.getValue();
+                System.out.println(object);
             }
 
-        } catch (UnsupportedEncodingException | FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        } while (status == Calc.YYPUSH_MORE);
+        if (status != Calc.YYACCEPT)
+            System.exit(1);
 
     }
 }
